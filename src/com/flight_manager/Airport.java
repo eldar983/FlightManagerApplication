20ca9c9e3ccad45ffbe5c56fe1803d18d5cc5900
Eldar Muminhodzic <eldar983@hotmail.com>
2019-02-03 00:21:27 +0100

    Initial commit

---
+++ src/com/flight_manager/Airport.java
@@ -0,0 +1,28 @@
+package com.flight_manager;
+
+public class Airport {
+	
+	private String name;
+	
+	
+	public Airport() {
+		
+	}
+	
+	public Airport(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Airport [name=" + name + "]";
+	}
+}
